e088837725d5cc52eb0ce679a892a37703f6b7db
princeoo635 <princeoo635@gmail.com>
2025-07-29 11:59:28 +0530

    minor fix

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import { app } from "./app.js";
 import dotenv from 'dotenv'
-
+import connectDB from './db/connect.js'; 
 dotenv.config({
     path:"./.env"
 })
 
-app.listen(process.env.PORT || 6000 , ()=>{
-    console.log(`server is running on PORT : ${process.env.PORT}`);
-    
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 6000 , ()=>{
+        console.log(`server is running on PORT : ${process.env.PORT}`);
+
+    })
 })
+.catch((error) => {
+    console.error("Failed to connect to the database:", error);
+    process.exit(1);
+});
