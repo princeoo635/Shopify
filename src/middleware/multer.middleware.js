16c1a82f7e3a4147170465b40962dc12b108001a
princeoo635 <princeoo635@gmail.com>
2025-07-30 10:01:38 +0530

    add multer middleware for file uploads

---
+++ src/middleware/multer.middleware.js
@@ -0,0 +1,11 @@
+import multer from 'multer'
+const storage=multer.diskStorage({
+    distination:function(req,file,cb){
+        cb(null,"./public/temp")
+    },
+    filename:function(req,file,cb){
+        cb(null,file.orininalname)
+    }
+})
+
+export const upload=multer({storage})
