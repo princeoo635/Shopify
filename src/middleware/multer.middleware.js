0042a0853809ccecea598115c285c343973b5ed9
princeoo635 <princeoo635@gmail.com>
2025-07-30 11:15:59 +0530

    fix: correct typo in multer filename and ensure Promise is capitalized in asyncHandler

--- src/middleware/multer.middleware.js
+++ src/middleware/multer.middleware.js
@@ -4,7 +4,7 @@ const storage=multer.diskStorage({
         cb(null,"./public/temp")
     },
     filename:function(req,file,cb){
-        cb(null,file.orininalname)
+        cb(null,'file.orininalname')
     }
 })
 
