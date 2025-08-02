90e22e9663bd7da64d976c14d6263896146635da
princeoo635 <princeoo635@gmail.com>
2025-08-02 18:28:09 +0530

    feat: add register route to home routes for user registration

--- src/routes/home.routes.js
+++ src/routes/home.routes.js
@@ -1,9 +1,10 @@
 import {Router} from 'express'
-import {home,login} from '../controller/home.controller.js'
+import {home,login,register} from '../controller/home.controller.js'
 
 const router=Router()
 
 router.route('/').get(home)
+router.route('/register').get(register)
 router.route('/login').get(login)
 
 export default router;
